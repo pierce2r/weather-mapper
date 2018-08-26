3729bd53bb043546b86b00ad574224e36df51220
Safian Blay <41937724+pierce2r@users.noreply.github.com>
2018-08-26 12:25:45 -0400

    Add files via upload

---
+++ weather_mapper.py
@@ -0,0 +1,48 @@
+import json
+import requests
+import datetime
+from playsound import playsound
+from gtts import gTTS
+
+city = raw_input('Please  enter city name:')
+#banner design
+print('*'*80)
+print('Created by Pierce2r').center(80)
+print('Weather-Mapper Version 2.0').center(80)
+print('*'*80)
+print('[*] Finding weather details for ' + city+ '...')
+
+api_key='*********' # put your api here
+url= 'http://api.openweathermap.org/data/2.5/weather?q='+str(city)+'&appid='+str(api_key)
+
+json_data=requests.get(url).json()
+try:
+	
+	main_data= json_data['main']['temp']
+	weathercal = main_data - 273.15
+		
+		
+
+
+	weather_data= json_data['weather'][0]['description']
+	country_data= json_data['sys']['country']
+	main1_data= json_data['main']['pressure']
+	main2_data= json_data['main']['humidity']
+	print('[+] Weather type: ' +weather_data)
+	print ('[+] Temperature: ' +str(weathercal))
+	print('[+] Pressure: ' +str(float(main1_data)))
+	print('[+] Humidity: ' +str(float(main2_data)))
+	print('[+] Country: ' +country_data)
+	result= 'temperature is '+str(weathercal)+' degree celsius in'+str(city)+' and with a weather type' +weather_data
+
+	#speech recognition
+	tts = gTTS(text=result, lang='en')
+	tts.save('/root/temp.mp3')
+	playsound('/root/temp.mp3')	
+	print (datetime.datetime.now())
+
+
+except KeyboardInterrupt:
+	print('exitting....')
+
+
